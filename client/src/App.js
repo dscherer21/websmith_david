02692f0f12dbf864e12e3b1879d934cdbf1f75f5
David Scherer-ODell <dscherer21@gmail.com>
2018-04-20 16:24:33 -0500

    added fonts and an iframe song to header

--- client/src/App.js
+++ client/src/App.js
@@ -7,13 +7,21 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Router>
         <header className='container'>
           <div className='row'>
             <div className='col-xs-6'>
-              <h1>Websmith David</h1>
+              <h1><span className='macondoFont'>Websmith David</span></h1>
             </div>
             <div className='col-xs-6'>
-              
+              <nav>
+                <h3><span className='macondoFont'>
+                  <Link>Front Counter</Link> | <Link>Forge/Wares</Link> | <Link>Messenger Raven</Link>
+                </span></h3>
+              </nav>
+            </div>
+            <div className='row text-center'>
+              <iframe width="50%" height="150" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/197093602&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
             </div>
           </div>
         </header>
@@ -25,7 +33,7 @@ class App extends Component {
         <footer>
 
         </footer>
-
+        </Router>
       </div>
     );
   }
