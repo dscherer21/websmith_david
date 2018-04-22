184e73a3efb2d525b7383e06d600051bc20f1fa6
David Scherer-ODell <dscherer21@gmail.com>
2018-04-22 02:49:32 -0500

    added stylings to panels on homepage, rewrote introduction message, and added borders for more metallic look and feel.

--- client/src/App.js
+++ client/src/App.js
@@ -22,7 +22,7 @@ class App extends Component {
               <div className='col-xs-6'>
                 <nav>
                   <h2><span className='macondoFont metallicLettering'>
-                    <Link to='/'>Store Counter</Link> | <Link to='#'>Forge/Wares</Link> | <Link to='#'>Messenger Raven</Link>
+                    <Link to='/'>Store Front</Link> | <Link to='#'>Forge/Wares</Link> | <Link to='#'>Messenger Raven</Link>
                   </span></h2>
                 </nav>
               </div>
@@ -31,22 +31,20 @@ class App extends Component {
 
           <main className='container'>
             <div className='row'>
-              <div clasName='col-xs-6'>
-                <h2>Welocome!</h2>
-                <p className='metallicLettering'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                <p className='metallicLettering'><quote>"As a highly skilled Websmith, I shall craft you a fine webpage made of 'Rare ReactJS Ore', slowly molded together with fine strokes of my 'Mighty NPM Hammer' upon the 'Anvil of HTML!'"  -Websmith David- </quote></p>
+              <div className='col-xs-6 panelPlate'>
+                <h2 className='macondoFont metallicLettering'>Welocome!</h2>
+                <p className='metallicLettering'>David is so glad that you are able to visit his forge today, Gallant Hero! That is David in the portrait. Behold, his mighty Websmith Beard! If you are interested in learning more about David, please stay here in the "Store Front" and scroll down and I shall tell you of his fervor. If you are interested in Davids past projects, please click on "Forge/Wares", and I will give you a guided tour of the forge. If you would like to relay a message to David, please call a "Messenger Raven" to do so.</p>
+                <div id='quote'>
+                  <p className='metallicLettering'>"As a highly skilled Websmith, I shall craft you a fine webpage made of 'Rare ReactJS Ore', slowly molded together with fine strokes of my 'Mighty NPM Hammer' upon the 'Anvil of HTML!'"  -Websmith David- </p>
+                </div>
               </div>
-              <div clasName='col-xs-6'>
-                <h2>Ye Olde Websmith</h2>
-                <img id='portrait' src='./images/glitterbeard.jpg' alt='Glitter Beard Portrait'/>
-                <h2>Crest of the Websmith Clan</h2>
+              <div className='col-xs-5 panelPlate'>
+                <h2 className='macondoFont metallicLettering'>Ye Olde Websmith</h2>
+                <img id='portrait' src='./images/glitterbeard1.jpg' alt='Glitter Beard Portrait'/>
+                <h2 className='macondoFont metallicLettering'>Crest of the Websmith Clan</h2>
                 <img id='crest' src='./images/websitelogo.png' alt='Website Logo'/>
               </div>
             </div>
-            <hr/>
-            <hr/>
-            <hr/>
-            <hr/>
             <hr/>
             <hr/>
             <hr/>
@@ -54,7 +52,7 @@ class App extends Component {
           </main>
 
           <footer className='container'>
-            <div className="text-center navbar navbar-fixed-bottom">
+            <div className="text-center navbar navbar-relative-bottom">
                 <div className="container-fluid">
                   <iframe width="40%" height="100" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/197093602&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
                   <h3>© 2018 Copyright</h3>
