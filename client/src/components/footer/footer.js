053f0223a736aeb6dc5cd8cd84a3063664ded408
David Scherer-ODell <dscherer21@gmail.com>
2018-05-02 15:04:26 -0500

    attempting another player fix

--- client/src/components/footer/footer.js
+++ client/src/components/footer/footer.js
@@ -1,14 +1,15 @@
 import React, { Component } from "react";
 import './footer.css';
 import { Link } from "react-router-dom";
+import ReactPlayer from 'react-player'
 
 const Footer = () => {
     return(
       <footer className='container'>
         <div className="text-center navbar navbar-relative-bottom">
             <div className="container-fluid">
-              <iframe src="https://www.youtube.com/embed/FxchcNLWpH4?autoplay=1" frameborder="0" allow="autoplay; encrypted-media" webkit-playsinline="true" playsinline="true"></iframe>
-              <h3>© 2018 Copyright</h3>
+              <ReactPlayer className='col-xs-12' url="https://www.youtube.com/embed/FxchcNLWpH4?autoplay=1" playing='true' loop='true' controls='true' webkit-playsinline="true" playsinline="true" width='60%' height='auto'/>
+              <h3 className='col-xs-12'>© 2018 Copyright</h3>
             </div>
         </div>
       </footer>
