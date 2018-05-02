dac752f98026147de5b62b36eb4b0df78fa8ef38
David Scherer-ODell <dscherer21@gmail.com>
2018-05-02 13:27:20 -0500

    fixing issue with iframe opening on fullscreen in mobile on play

--- client/src/components/footer/footer.js
+++ client/src/components/footer/footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
       <footer className='container'>
         <div className="text-center navbar navbar-relative-bottom">
             <div className="container-fluid">
-              <iframe src="https://www.youtube.com/embed/FxchcNLWpH4?autoplay=1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+              <iframe src="https://www.youtube.com/embed/FxchcNLWpH4?autoplay=1" frameborder="0" allow="autoplay; encrypted-media" webkit-playsinline></iframe>
               <h3>© 2018 Copyright</h3>
             </div>
         </div>
