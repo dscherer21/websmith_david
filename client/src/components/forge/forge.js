277db548b61c64975060241735e620bbbc77c21c
David Scherer-ODell <dscherer21@gmail.com>
2018-08-30 03:52:16 -0500

    worked all forgeitems into a constructor function and added websmith-david project and Holy Covenant UMC

--- client/src/components/forge/forge.js
+++ client/src/components/forge/forge.js
@@ -19,216 +19,117 @@ const Forge = () => {
         <div className='row'>
           <div className='col-xs-12'>
             <div className='clearfix panelPlate placardText'>
-              <h2 className='macondoFont metallicLettering'>David{"'"}s Wares</h2>
-              {/*Portfolio Works*/}
-              <Forgeitem/>
-              <div className="col-xs-12 col-sm-12 col-md-12">
+                <h2 className='macondoFont metallicLettering'>David{"'"}s Wares</h2>
+                {/*Portfolio Works*/}
+
+                {/*Websmith David*/}
+                <Forgeitem
+                    projectLink={"https://websmith-david.herokuapp.com/"}
+                    imgSrc={"./images/websmithdavidscreenshot.png"}
+                    projectTitle={"Websmith David"}
+                    projectDescription={"David's Main Portfolio page."}
+                    technologiesUsed={"HTML5, CSS3, JavaScript, Bootstrap3, ES6, JSX, ReactJS, ExpressJS, NodeJS, Body-Parser, NodeMailer, DOTENV, Concurrently, Nodemon, Axios, React-dom, react-player, react-captcha, react-router-dom, react-scripts, and Google Fonts."}
+                />
+
+                {/*ReactJS NYT Search*/}
+                <Forgeitem
+                    projectLink={"https://nyt-mern-stack.herokuapp.com/"}
+                    imgSrc={"./images/nytscreenshot.png"}
+                    projectTitle={"ReactJS NYT Search"}
+                    projectDescription={"A Full MERN Stack Application that searches for articles from The New York Times Website, allows you to store them in a database, and make comments on them."}
+                    technologiesUsed={"HTML5, CSS3, JavaScript, NodeJs, Yarn, ES6, Bootstrap 3, JQuery, ExpressJS, MongoDB, ReactJS, Axios, Body-Parser, Dotenv, Morgan, Mongoose, Mongojs, React-Dom, React-Router-Dom, React-Scripts"}
+                />
+
+                {/*Holy Covenant UMC*/}
+                <Forgeitem
+                    projectLink={"https://www.holycovenantumc.org/"}
+                    imgSrc={"./images/hcumcscreenshot.png"}
+                    projectTitle={"Holy Covenant UMC"}
+                    projectDescription={"David consults with church elders once a month about changes to the site, and helps to implement them."}
+                    technologiesUsed={"The site used WordPress in the past, but has since upgraded to Squarespace."}
+                />
+
                 {/*DateNight*/}
-                <div className='container quote'>
-                    <div className='row'>
+                <Forgeitem
+                    projectLink={"https://dscherer21.github.io/datenight"}
+                    imgSrc={"./images/app-image.png"}
+                    projectTitle={"DateNight"}
+                    projectDescription={'This app is intended to help you easily plan your next night out at the movies. DateNight streamlines the process of planning your date by combining 2 processes into 1; picking a movie, theater and showtime and also a restaurant to grab a bite to eat before or after the movie.'}
+                    technologiesUsed={"HTML5, CSS3, Bootstrap 3, International Showtimes API, Google Maps, Places and Geocodes API, Javascript, and jQuery"}
+                />
 
-                        <Link to="https://dscherer21.github.io/datenight" className='flip col-xs-12 col-sm-12 col-md-4' target="_blank">
-                            <img className='img img-thumbnail' src="./images/app-image.png" alt="DateNight"/>
-                            <h3 className="appLabels">DateNight</h3>
-                        </Link>
-                        
-                        <div className='col-xs-12 col-sm-12 col-md-4'>
-                            <h2>Description:</h2>
-                            <p>This app is intended to help you easily plan your next night out at the movies. DateNight streamlines the process of planning your date by combining 2 processes into 1; picking a movie, theater and showtime and also a restaurant to grab a bite to eat before or after the movie.</p>
-                        </div>
-                        
-                        <div className='col-xs-12 col-sm-12 col-md-4'>
-                            <h2>Technologies Used:</h2>
-                            <p>HTML5, CSS3, Bootstrap 3, International Showtimes API, Google Maps, Places and Geocodes API, Javascript, and jQuery</p>
-                        </div>
-
-                    </div>
-                </div>
-              </div>
-              <div className="col-xs-12 col-sm-12 col-md-12">
                 {/*Bootstrap Portfolio*/}
-                <div className='container quote'>
-                    <div className='row'>
+                <Forgeitem
+                    projectLink={"https://dscherer21.github.io/bootstrap-portfolio"}
+                    imgSrc={"./images/bootstrapportfolioscreenshot.png"}
+                    projectTitle={"Bootstrap Portfolio"}
+                    projectDescription={'This app is my first original portfolio page. It Has a short description about myself and my projects.'}
+                    technologiesUsed={"HTML5, CSS3, Bootstrap 3, and JavaScript"}
+                />
 
-                        <Link to="https://dscherer21.github.io/bootstrap-portfolio" className='flip col-xs-12 col-sm-12 col-md-4' target="_blank">
-                            <img className='img img-thumbnail' src="./images/bootstrapportfolioscreenshot.png" alt="Bootstrap Portfolio Screenshot"/>
-                            <h3 className="appLabels">Bootstrap Portfolio</h3>
-                        </Link>
-                        
-                        <div className='col-xs-12 col-sm-12 col-md-4'>
-                            <h2>Description:</h2>
-                            <p>This app is my first original portfolio page. It Has a short description about myself and my projects.</p>
-                        </div>
-                        
-                        <div className='col-xs-12 col-sm-12 col-md-4'>
-                            <h2>Technologies Used:</h2>
-                            <p>HTML5, CSS3, Bootstrap 3, and JavaScript</p>
-                        </div>
-
-                    </div>
-                </div>
-              </div>
-              <div className="col-xs-12 col-sm-12 col-md-12">
                 {/*DevTech Scraper*/}
-                <div className='container quote'>
-                    <div className='row'>
+                <Forgeitem
+                    projectLink={"https://devtechscraper.herokuapp.com/"}
+                    imgSrc={"./images/devtechscreenshot.png"}
+                    projectTitle={"DevTech Scraper"}
+                    projectDescription={'An app that scrapes the www.developer-tech.com website for articles, stores them in a MongoDB database, and displays them on a webpage.'}
+                    technologiesUsed={"HTML5, CSS3, JavaScript, jQuery, Bootstrap 3, Axios, Body-Parser, Cheerio, Dotenv, ExpressJS, Express-Handlebars, Mongojs, Mongoose, Morgan, Nodemon, Request, NodeJS, NPM, and MongoDB"}
+                />
 
-                        <Link to="https://devtechscraper.herokuapp.com/" className='flip col-xs-12 col-sm-12 col-md-4' target="_blank">
-                            <img className='img img-thumbnail' src="./images/devtechscreenshot.png" alt="DevTech Scraper Screenshot"/>
-                            <h3 className="appLabels">DevTech Scraper</h3>
-                        </Link>
-                        
-                        <div className='col-xs-12 col-sm-12 col-md-4'>
-                            <h2>Description:</h2>
-                            <p>An app that scrapes the <Link to='http://www.developer-tech.com' target='_blank'>www.developer-tech.com</Link> website for articles, stores them in MongoDB database, and displays them on a webpage.</p>
-                        </div>
-                        
-                        <div className='col-xs-12 col-sm-12 col-md-4'>
-                            <h2>Technologies Used:</h2>
-                            <p>HTML5, CSS3, JavaScript, jQuery, Bootstrap 3, Axios, Body-Parser, Cheerio, Dotenv, ExpressJS, Express-Handlebars, Mongojs, Mongoose, Morgan, Nodemon, Request, NodeJS, NPM, and MongoDB</p>
-                        </div>
-
-                    </div>
-                </div>
-              </div>
-              <div className="col-xs-12 col-sm-12 col-md-12">
                 {/*The Matrix Hangman*/}
-                <div className='container quote'>
-                    <div className='row'>
+                <Forgeitem
+                    projectLink={"https://dscherer21.github.io/Hangman-Game/"}
+                    imgSrc={"./images/matrixwallpaper.jpg"}
+                    projectTitle={"The Matrix Hangman"}
+                    projectDescription={'An app that lets you play a Matrix themed game of "Hangman."'}
+                    technologiesUsed={"HTML5, CSS3, Bootstrap 3, JavaScript, and GoogleFonts"}
+                />
 
-                        <Link to="https://dscherer21.github.io/Hangman-Game/" className='flip col-xs-12 col-sm-12 col-md-4' target="_blank">
-                            <img className='img img-thumbnail' src="./images/matrixwallpaper.jpg" alt="Hangman"/>
-                            <h3 className="appLabels">The Matrix Hangman</h3>
-                        </Link>
-                        
-                        <div className='col-xs-12 col-sm-12 col-md-4'>
-                            <h2>Description:</h2>
-                            <p>An app that lets you play a Matrix themed game of "Hangman."</p>
-                        </div>
-                        
-                        <div className='col-xs-12 col-sm-12 col-md-4'>
-                            <h2>Technologies Used:</h2>
-                            <p>HTML5, CSS3, Bootstrap 3, JavaScript, and GoogleFonts</p>
-                        </div>
-
-                    </div>
-                </div>
-              </div>
-              <div className="col-xs-12 col-sm-12 col-md-12">
                 {/*Futurama RPG*/}
-                <div className='container quote'>
-                    <div className='row'>
+                <Forgeitem
+                    projectLink={"https://dscherer21.github.io/futurama-rpg-game/"}
+                    imgSrc={"./images/futuramabackground.jpg"}
+                    projectTitle={"Futurama RPG"}
+                    projectDescription={'A mini RPG(Roleplaying Game) based around the cartoon show "Futurama."'}
+                    technologiesUsed={"HTML5, CSS3, Bootstrap 3, JavaScript, jQuery, and GoogleFonts"}
+                />
 
-                        <Link to="https://dscherer21.github.io/futurama-rpg-game/" className='flip col-xs-12 col-sm-12 col-md-4' target="_blank">
-                            <img className='img img-thumbnail' src="./images/futuramabackground.jpg" alt="Futurama RPG"/>
-                            <h3 className="appLabels">Futurama RPG</h3>
-                        </Link>
-                        
-                        <div className='col-xs-12 col-sm-12 col-md-4'>
-                            <h2>Description:</h2>
-                            <p>A mini RPG game based around the cartoon show "Futurama."</p>
-                        </div>
-                        
-                        <div className='col-xs-12 col-sm-12 col-md-4'>
-                            <h2>Technologies Used:</h2>
-                            <p>HTML5, CSS3, Bootstrap 3, JavaScript, jQuery, and GoogleFonts</p>
-                        </div>
-
-                    </div>
-                </div>
-              </div>
-              <div className="col-xs-12 col-sm-12 col-md-12">
                 {/*Dexter Trivia*/}
-                <div className='container quote'>
-                    <div className='row'>
+                <Forgeitem
+                    projectLink={"https://dscherer21.github.io/dexter-trivia-game/"}
+                    imgSrc={"./images/dexterbackground.jpg"}
+                    projectTitle={"Dexter Trivia"}
+                    projectDescription={'A timed trivia game based around the HBO TV show "Dexter."'}
+                    technologiesUsed={"HTML5, CSS3, Bootstrap 3, JavaScript, jQuery, and GoogleFonts"}
+                />
 
-                        <Link to="https://dscherer21.github.io/dexter-trivia-game/" className='flip col-xs-12 col-sm-12 col-md-4' target="_blank">
-                            <img className='img img-thumbnail' src="./images/dexterbackground.jpg" alt="Dexter Trivia"/>
-                            <h3 className="appLabels">Dexter Trivia</h3>
-                        </Link>
-                        
-                        <div className='col-xs-12 col-sm-12 col-md-4'>
-                            <h2>Description:</h2>
-                            <p>A timed trivia game based around the HBO TV show "Dexter."</p>
-                        </div>
-                        
-                        <div className='col-xs-12 col-sm-12 col-md-4'>
-                            <h2>Technologies Used:</h2>
-                            <p>HTML5, CSS3, Bootstrap 3, JavaScript, jQuery, and GoogleFonts</p>
-                        </div>
-
-                    </div>
-                </div>
-              </div>
-              <div className="col-xs-12 col-sm-12 col-md-12">
                 {/*Super Hero GIF Widget*/}
-                <div className='container quote'>
-                    <div className='row'>
+                <Forgeitem
+                    projectLink={"https://dscherer21.github.io/super-hero-gif-search-widget/"}
+                    imgSrc={"./images/comicbookwallpaper.jpg"}
+                    projectTitle={"Super Hero GIF Widget"}
+                    projectDescription={'An app that has some Hero Buttons that, when clicked on, generate GIFs related to that Hero. You can also create your own Hero Button with the "Create Hero Button" button.'}
+                    technologiesUsed={"HTML5, CSS3, Bootstrap 3, JavaScript, jQuery, and GoogleFonts"}
+                />
 
-                        <Link to="https://dscherer21.github.io/super-hero-gif-search-widget/" className='flip col-xs-12 col-sm-12 col-md-4' target="_blank">
-                            <img className='img img-thumbnail' src="./images/comicbookwallpaper.jpg" alt="Super Hero GIF  Widget"/>
-                            <h3 className="appLabels">Super Hero GIF Widget</h3>
-                        </Link>
-                        
-                        <div className='col-xs-12 col-sm-12 col-md-4'>
-                            <h2>Description:</h2>
-                            <p>An app that has some Hero Buttons that, when clicked on, generate GIFs related to that Hero. You can also create your own Hero Button with the "Create Hero Button" button.</p>
-                        </div>
-                        
-                        <div className='col-xs-12 col-sm-12 col-md-4'>
-                            <h2>Technologies Used:</h2>
-                            <p>HTML5, CSS3, Bootstrap 3, JavaScript, jQuery, and GoogleFonts</p>
-                        </div>
-
-                    </div>
-                </div>
-              </div>
-              <div className="col-xs-12 col-sm-12 col-md-12">
                 {/*BamazonJS*/}
-                <div className='container quote'>
-                    <div className='row'>
+                <Forgeitem
+                    projectLink={"https://github.com/dscherer21/bamazon"}
+                    imgSrc={"./images/customerdemo1.png"}
+                    projectTitle={"BamazonJS"}
+                    projectDescription={'An Amazon Mock up run from the Command Line. Three apps running together: A Customer Side app, A manager app, and an Executive app. This is not a deployed application, however, you can view a YouTube video of me demoing it on Github.'}
+                    technologiesUsed={"JavaScript, MySQL, NodeJS, NPM, Cli-table, Colors, Dotenv, Inquirer, and Prompt"}
+                />
 
-                        <Link to="https://github.com/dscherer21/bamazon" className='flip col-xs-12 col-sm-12 col-md-4' target="_blank">
-                            <img className='img img-thumbnail' src="./images/customerdemo1.png" alt="BamazonJS Screenshot"/>
-                            <h3 className="appLabels">BamazonJS</h3>
-                        </Link>
-                        
-                        <div className='col-xs-12 col-sm-12 col-md-4'>
-                            <h2>Description:</h2>
-                            <p>An Amazon Mock up run from the Command Line. Three apps running together: A Customer Side app, A manager app, and an Executive app. This is not a deployed application, however, you can view a YouTube video of me demoing it --{'>'}<Link to='https://youtu.be/x_BvqZW0kIM' target='_blank'>Here</Link>.</p>
-                        </div>
-                        
-                        <div className='col-xs-12 col-sm-12 col-md-4'>
-                            <h2>Technologies Used:</h2>
-                            <p>JavaScript, MySQL, NodeJS, NPM, Cli-table, Colors, Dotenv, Inquirer, and Prompt</p>
-                        </div>
-
-                    </div>
-                </div>
-              </div>
-              <div className="col-xs-12 col-sm-12 col-md-12">
                 {/*Heroes Friend Finder*/}
-                <div className='container quote'>
-                    <div className='row'>
-
-                        <Link to="https://heroesfriendfinder.herokuapp.com/" className='flip col-xs-12 col-sm-12 col-md-4' target="_blank">
-                            <img className='img img-thumbnail' src="./images/friendfinderscreenshot.png" alt="Heroes Friend Finder Screenshot"/>
-                            <h3 className="appLabels">Heroes Friend Finder</h3>
-                        </Link>
-                        
-                        <div className='col-xs-12 col-sm-12 col-md-4'>
-                            <h2>Description:</h2>
-                            <p>In this app, you will be asked a series of questions. Depending on your answers, you will be paired with another person. Your name and answers will be stored in the database, and anyone that answers similarly will be paired with you. There is already 8 people, characters from the hit TV series "Heroes", stored in the database.</p>
-                        </div>
-                        
-                        <div className='col-xs-12 col-sm-12 col-md-4'>
-                            <h2>Technologies Used:</h2>
-                            <p>HTML5, CSS3, JavaScript, Bootstrap 3, ExpressJS, Body-Parser, Self-made API, JSON, NodeJS, NPM, and Path</p>
-                        </div>
-
-                    </div>
-                </div>
-              </div>
+                <Forgeitem
+                    projectLink={"https://heroesfriendfinder.herokuapp.com/"}
+                    imgSrc={"./images/friendfinderscreenshot.png"}
+                    projectTitle={"Heroes Friend Finder"}
+                    projectDescription={'In this app, you will be asked a series of questions. Depending on your answers, you will be paired with another person. Your name and answers will be stored in the database, and anyone that answers similarly will be paired with you. There is already 8 people, characters from the hit TV series "Heroes", stored in the database.'}
+                    technologiesUsed={"HTML5, CSS3, JavaScript, Bootstrap 3, ExpressJS, Body-Parser, Self-made API, JSON, NodeJS, NPM, and Path"}
+                />
+              
             </div>
           </div>
         </div>
--- client/src/components/forge/forgeitem.js
+++ client/src/components/forge/forgeitem.js
@@ -4,29 +4,29 @@ import { Link } from "react-router-dom";
 
 class Forgeitem extends Component {
     constructor() {
-    super()
+        super()
+        
     } 
     render() {
-    return(
+        return(
 
             <div className="col-xs-12 col-sm-12 col-md-12">
-            {/*NYT Article Search*/}
                 <div className='container quote'>
                     <div className='row'>
 
-                        <Link to="https://nyt-mern-stack.herokuapp.com/" className='flip col-xs-12 col-sm-12 col-md-4' target="_blank">
-                            <img className='img img-thumbnail' src="./images/nytscreenshot.png" alt="NYT Article Search"/>
-                            <h3 className="appLabels">ReactJS NYT Search</h3>
+                        <Link to={this.props.projectLink} className='flip col-xs-12 col-sm-12 col-md-4' target="_blank">
+                            <img className='img img-thumbnail' src={this.props.imgSrc} alt={this.props.projectTitle}/>
+                            <h3 className="appLabels">{this.props.projectTitle}</h3>
                         </Link>
                         
                         <div className='col-xs-12 col-sm-12 col-md-4'>
                             <h2>Description:</h2>
-                            <p>A Full MERN Stack Application that searches for articles from "The New York Times" Website, allows you to store them in a database, and make comments on them.</p>
+                            <p>{this.props.projectDescription}</p>
                         </div>
                         
                         <div className='col-xs-12 col-sm-12 col-md-4'>
                             <h2>Technologies Used:</h2>
-                            <p>HTML5, CSS3, JavaScript, NodeJs, Yarn, ES6, Bootstrap 3, JQuery, ExpressJS, MongoDB, ReactJS, Axios, Body-Parser, Dotenv, Morgan, Mongoose, Mongojs, React-Dom, React-Router-Dom, React-Scripts</p>
+                            <p>{this.props.technologiesUsed}</p>
                         </div>
 
                     </div>
